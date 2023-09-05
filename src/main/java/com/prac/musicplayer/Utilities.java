23f3b87489c279fa0224beb796977003bfb7dac1
Tobechi00 <tobechiokaro2013@gmail.com>
2023-09-05 09:20:04 +0100

    minor changes
    changed class names and refactored code, added further separation between controller and view

--- src/main/java/com/prac/musicplayer/Utilities.java
+++ src/main/java/com/prac/musicplayer/Utilities.java
@@ -13,17 +13,19 @@ import java.util.stream.Stream;
 public class Utilities {
     public static ObservableList<String> listFilesForFolder(Path path) {
         File folder = path.toFile();
-            return FXCollections.observableList(Stream.of(folder.listFiles())
-                    .filter(file -> !file.isDirectory())
-                    .map(File::getName)
-                    .collect(Collectors.toList()));
+        return FXCollections.observableList(Stream.of(folder.listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toList()));
     }
-    public static File combine(Path path1, Path path2){
+
+    public static File combine(Path path1, Path path2) {
         File file = new File(path1.toUri());
 
-        return new File(file,path2.toString());
+        return new File(file, path2.toString());
     }
-    public static String millsToMins(long time){
+
+    public static String millsToMins(long time) {
         Format minFormat = new SimpleDateFormat("mm:ss");
         return minFormat.format(time);
     }
